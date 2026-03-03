9a44aed7a26abe593b672d48c84b07c365a095cf
Chidambar P Bangre <chidambarpb@gmail.com>
2026-03-03 23:37:59 +0530

    modified tf files

--- Eval-3-Question/backend.tf
+++ Eval-3-Question/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "Chidu-RG"
-    storage_account_name = "eval4adosa"
-    container_name       = "terraformado"
-    key                  = "evalq3.tfstate"
+    # resource_group_name  = "Chidu-RG"
+    # storage_account_name = "eval4adosa"
+    # container_name       = "terraformado"
+    # key                  = "evalq3.tfstate"
   }
 }
