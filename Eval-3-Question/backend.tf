785aed6e1729b9ad6f58e6f0e07b63d158ed3e43
Chidambar P Bangre <chidambarpb@gmail.com>
2026-03-03 22:43:32 +0530

    updated tf config files

--- Eval-3-Question/backend.tf
+++ Eval-3-Question/backend.tf
@@ -1,5 +1,8 @@
 terraform {
   backend "azurerm" {
-    
+    resource_group_name  = "Chidu-RG"
+    storage_account_name = "eval4adosa"
+    container_name       = "terraformado"
+    key                  = "evalq3.tfstate"
   }
 }
--- Eval-3-Question/module-github/main.tf
+++ Eval-3-Question/module-github/main.tf
@@ -1,3 +1,8 @@
 module "network_vm" {
-  source = "git::https://github.com/Chidu0665/Eval-3-Question.git"
+  source = "git@github.com:Chidu0665/Eval-3-Question.git"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key"
+  type        = string
 }
